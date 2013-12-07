a16b8528711d08a7ca2e4319745b9f4b81074802
kyo-ago <kyo@0-9.sakura.ne.jp>
2013-12-08 02:37:59 +0900

    add mixed test.

---
+++ test/fixtures/mixed/index.js
@@ -0,0 +1,49 @@
+/*
+ * Base license
+ *
+ * Copyright (c) 2013 kyo_ago
+ * Licensed under the MIT license.
+ */
+alert(1);
+//     Backbone.js 1.1.0
+
+//     (c) 2010-2011 Jeremy Ashkenas, DocumentCloud Inc.
+//     (c) 2011-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+//     Backbone may be freely distributed under the MIT license.
+//     For all details and documentation:
+//     http://backbonejs.org
+
+(function(){
+
+  // Initial Setup
+  // -------------
+
+  // Save a reference to the global object (`window` in the browser, `exports`
+  // on the server).
+  var root = this;
+
+}).call(this);
+// inline Copyright (c) 2013 kyo_ago Licensed under the MIT license.
+alert(1);
+//     Underscore.js 1.5.2
+//     http://underscorejs.org
+//     (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+//     Underscore may be freely distributed under the MIT license.
+
+(function(){
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `exports` on the server.
+  var root = this;
+
+  // Save the previous value of the `_` variable.
+  var previousUnderscore = root._;
+
+}).call(this);
+/* Zepto v1.1 - zepto event ajax form ie - zeptojs.com/license */
+
+
+var Zepto = (function() {
+})(Zepto)
